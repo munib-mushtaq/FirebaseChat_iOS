fd306fbefaf309d095feafd45055d3ae47eae4ab
Arslan Raza <arslan.raza@invozone.com>
2023-11-22 10:40:53 +0500

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Arslan Raza on 22/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
